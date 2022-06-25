2695852d5721465a8b659b101da7056b56e89dd6
tsizzle123 <mcleodtariq@gmail.com>
2022-06-24 17:33:16 -0700

    Create File_Cutter.py

---
+++ File_Cutter.py
@@ -0,0 +1,27 @@
+import pandas as pd,ftplib,requests,os
+
+df = pd.read_csv('C:/Users/tarmcleod/Downloads/Medallia_Cases_Master.csv')
+path = 'C:/Users/tarmcleod/Downloads/Medallia_Chunks/chunk{}.csv'
+file_size = df.memory_usage(index=True, deep=False).sum() / 1048576
+print(file_size)
+
+
+def save_file_part(df,size_threshold,save_path,part_number=0):
+        file_size = df.memory_usage(index=True,deep=False).sum() / 1048576
+        num_records = len(df)
+
+        if file_size > size_threshold:
+            records_to_split = int(num_records * size_threshold // file_size)
+            df_to_keep = df.head(records_to_split)
+            print(df_to_keep.memory_usage(index=True,deep=False).sum() / 1048576)
+            df_to_keep.to_csv(save_path.format(part_number),index=False)
+            save_file_part(df.tail(num_records-records_to_split), size_threshold,path,part_number=part_number+1)
+
+        else:
+            df.to_csv(save_path.format(part_number),index=False)
+
+
+save_file_part(df,.3,path)
+
+
+
